fc50c2d2094bd36a73a4a6d29d0787ed38bd04c3
philusdevs <philusdevs@gmail.com>
2023-07-27 16:19:47 +0200

    Changes to be committed: 	modified:   gatsby-browser.js 	deleted:    gatsby-ssr.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { ThemeProvider } from './src/ThemeContext/ThemeContext';
-
 import './src/components/prism-coldark-dark.css'; // Move the import inside gatsby-browser.js
 
 export const wrapRootElement = ({ element }) => (
