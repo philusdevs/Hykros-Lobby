8b0eb241adbb8a72b8ddba856e5ea42fd94d2dc2
philusdevs <philusdevs@gmail.com>
2023-11-28 22:08:01 +0200

    added theme switch

--- src/components/header.js
+++ src/components/header.js
@@ -95,7 +95,7 @@ const Header = () => {
             );
           })}
           <HeaderNavListItem>
-            
+            <ThemeSwitchButton />
           </HeaderNavListItem>
         </HeaderNavList>
       </HeaderWrapper>
